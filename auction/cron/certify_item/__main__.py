1411f1c1f8a83148b0af8e5f3d0f552532e336db
Luis Banegas Saybe <luis@trois.io>
2021-05-02 11:03:52 -0400

    feat(kubernetes)

--- auction/cron/certify_item/__main__.py
+++ auction/cron/certify_item/__main__.py
@@ -3,16 +3,19 @@ from auction.utility.db import getConnectionPool
 
 
 async def process_next_item(connection):
-    item = await connection.fetchrow('''
-        SELECT   item.*
-        FROM     item
-        WHERE    item.auction_end_date < now()
-        AND      item.highest_bidder IS NULL
-        ORDER BY item.auction_end_date ASC
-        LIMIT    1
-    ''')
+    def getNextItem():
+        return await connection.fetchrow('''
+            SELECT   item.*
+            FROM     item
+            WHERE    item.auction_end_date < now()
+            AND      item.highest_bidder IS NULL
+            ORDER BY item.auction_end_date ASC
+            LIMIT    1
+        ''')
 
-    if item:
+    item = getNextItem()
+
+    while item:
         highest_bid = await connection.fetchrow('''
             SELECT *
             FROM bid
@@ -21,20 +24,21 @@ async def process_next_item(connection):
         ''', item['id'])
 
         highest_bidder = highest_bid['user_id'] if highest_bid else -1
+
         await connection.execute('''
             UPDATE item SET
             highest_bidder = $2
             WHERE item.id = $1
         ''', item['id'], highest_bidder)
 
+        item = getNextItem()
+
 
 async def main():
     pool = await getConnectionPool()
 
-    while True:
-        async with pool.acquire() as connection:
-            await process_next_item(connection)
-        await asyncio.sleep(10)
+    async with pool.acquire() as connection:
+        await process_next_item(connection)
 
 
 asyncio.get_event_loop().run_until_complete(main())
